030804f8cbad70e7e1ac7e4be2f9d0e4d5265b09
Lisandro Rafaelano <lisandro.rafaelano@pluto.tv>
2025-05-21 15:30:17 -0600

    Adding secondary ip ranges

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,32 @@
 resource "google_compute_network" "vpc_network_gke" {
   project                 = var.project_id
-  name                    = var.name
+  name                    = "vpc-${var.name}"
   auto_create_subnetworks = false
   routing_mode            = var.routing_mode_vpc
-  delete_default_routes_on_create = true
 }
+
+
+resource "google_compute_subnetwork" "name" {
+  name = "sunet-${var.name}"
+  ip_cidr_range = var.gcp_cidr_subnet
+  network = google_compute_network.vpc_network_gke.id
+  project = var.project_id
+  region = "us-east1"
+  private_ip_google_access = true # Allow private access to Google APIs
+
+  secondary_ip_range {
+    range_name    = "k8s-pods"
+    ip_cidr_range = var.cidr_k8s_pods
+  }
+  secondary_ip_range {
+    range_name    = "k8s-services"
+    ip_cidr_range = var.cidr_k8s_services
+  }
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling = 0.5
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+
+  depends_on = [google_compute_network.vpc_network_gke]
+  }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -12,3 +12,18 @@ variable "routing_mode_vpc" {
   type        = string
   description = "dynamic routing mode that determines how routes learned from Cloud Routers are applied within the network, with options for Regional or Global routing"
 }
+
+variable "gcp_cidr_subnet" {
+  type        = string
+  description = "CIDR range for the subnet"
+}
+
+variable "cidr_k8s_pods" {
+  type        = string
+  description = "CIDR range for the k8s pods"
+}
+
+variable "cidr_k8s_services" {
+  type        = string
+  description = "CIDR range for the k8s services"
+}
