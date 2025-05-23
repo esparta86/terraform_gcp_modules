69133b374630052beadd06bffb8726fec161e3bc
Lisandro Rafaelano <lisandro.rafaelano@pluto.tv>
2025-05-23 10:13:12 -0600

    nat added

--- vpc/main.tf
+++ vpc/main.tf
@@ -42,3 +42,14 @@ resource "google_compute_subnetwork" "name" {
     }
 
   }
+
+
+  resource "google_compute_router_nat" "nat" {
+    name = "nat-${var.name}"
+    router = google_compute_router.router.name
+    region = google_compute_subnetwork.name.region
+    project = var.project_id
+    nat_ip_allocate_option = "AUTO_ONLY"
+    source_subnetwork_ip_ranges_to_nat = ["ALL_SUBNETWORKS_ALL_IP_RANGES"]
+    min_ports_per_vm = 64
+  }
