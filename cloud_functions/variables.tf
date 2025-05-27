2f35eb0e55eb72ff30247f4a07bfcfdb8fe7b630
Lisandro Rafaelano <lisandro.rafaelano@pluto.tv>
2025-05-27 15:06:58 -0600

    changes

--- cloud_functions/variables.tf
+++ cloud_functions/variables.tf
@@ -3,3 +3,7 @@ variable "bucket_name_cloud_function" {
   description = "Name of the Cloud Function storage bucket" 
 }
 
+variable "cloud_fn_map" {
+  type        = list(map(any))
+  description = "List of cloud functions with their properties"
+}
